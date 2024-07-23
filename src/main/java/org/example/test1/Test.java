6a94cc7990424b0c5f8a04bbf713447483cc670a
shao-mingming <728591892@qq.com>
2024-07-23 16:32:34 +0800

    代码的每一次提交都会生成一个版本 提示信息是用来给别人看的 你需要告诉团队成员 你更新了哪些 优化了哪些代码

---
+++ src/main/java/org/example/test1/Test.java
@@ -0,0 +1,74 @@
+package org.example.test1;
+
+import org.example.dao.StudentDAO;
+import org.example.dao.impl.StudentDAOImpl;
+import org.example.pojo.Student;
+
+import java.util.Date;
+
+/** JDBC java database connection
+ * @author SHAO
+ * @date 2024/7/22 下午2:47
+ */
+public class Test {
+    public static void main(String[] args) {
+//        // 驱动加载
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        System.out.println("驱动加载成功！");
+//
+//        // 获取jdbc连接
+//        String url ="jdbc:mysql://localhost:3306/nuist?characterEncoding=utf-8";
+//        String username = "root";
+//        String password = "a12345";
+//        Connection connection = null;
+//        try {
+//             connection
+//                    = DriverManager.getConnection(url,username,password);
+//        }catch(SQLException e){
+//            e.printStackTrace();
+//            }
+//        System.out.println("获取连接成功！");
+//
+//        try{
+//            Statement s = connection.createStatement();
+//            // 准备一个SQL语句
+//            String sql = "insert into stu " +
+//                    "(NAME,gender,birthday,addr,qqnumber)" +
+//                    "VALUES" +
+//                    "('刘','女','2006-02-15','江苏宿迁',3124076825)";
+//            // 运行sql语句
+//            s.execute(sql);
+//        } catch(SQLException e){
+//            e.printStackTrace();
+//            // 驱动加载 连接获取 创建编译语句对象 执行语句
+//            // 驱动加载 连接获取 创建编译语句对象 执行语句 获取查询结果集
+//        }
+
+
+
+//        Student s1 = new Student(0,"无畏","男",new Date()
+//                        ,"南京",110);
+        StudentDAO studentDAO = new StudentDAOImpl();
+//        studentDAO.insert(s1);
+
+        Student s2 = new Student(6,"不留情","女",
+                new Date(),"宿迁",119L);
+        studentDAO.update(s2);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
